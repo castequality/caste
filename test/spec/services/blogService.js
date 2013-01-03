b5b220eae240c442e59c77c757c89b0640d5cf5d
Sean Doyle <sean.p.doyle24@gmail.com>
2013-01-03 13:24:38 -0500

    mocked http calls in unit tests for #6. made column 800px wide

--- test/spec/services/blogService.js
+++ test/spec/services/blogService.js
@@ -1,18 +1,22 @@
 'use strict';
 
-describe('Service: blogService', function () {
+describe('Service: blogService', function (API_KEY, WEB_ROOT) {
 
   // load the service's module
   beforeEach(module('casteApp'));
 
   // instantiate service
-  var blogService;
-  beforeEach(inject(function(_blogService_) {
-    blogService = _blogService_;
+  var blogService, $httpBackend, injector;
+  beforeEach(inject(function($injector) {
+    injector = $injector;
   }));
 
   it('should retrieve blog posts', function () {
-    expect(!!blogService).toBe(true);
-  });
+    $httpBackend = injector.get('$httpBackend');
+    $httpBackend.whenJSONP(WEB_ROOT + '/posts').respond({
 
+    });
+    blogService = injector.get('blogService');
+    expect(blogService).toBe(true);
+  });
 });
--- test/spec/services/instagramService.js
+++ test/spec/services/instagramService.js
@@ -1,18 +1,33 @@
 'use strict';
 
-describe('Service: instagramService', function () {
+describe('Service: instagramService', function (INSTA_WEB_ROOT, INSTA_ID) {
 
   // load the service's module
   beforeEach(module('casteApp'));
 
   // instantiate service
-  var instagramService;
-  beforeEach(inject(function(_instagramService_) {
-    instagramService = _instagramService_;
+  var injector, $httpBackend, instagramService,
+  USER_URL = INSTA_WEB_ROOT + '/' + INSTA_ID,
+  FEED_URL = USER_URL + '/media/recent';
+  beforeEach(inject(function($injector) {
+    injector = $injector;
   }));
 
-  it('should do something', function () {
-    expect(!!instagramService).toBe(true);
+  it('should retrieve photos from the feed', function () {
+    $httpBackend = injector.get('$httpBackend');
+    $httpBackend.whenJSONP(USER_URL).respond({
+
+    });
+    instagramService = injector.get('instagramService');
+    expect(instagramService.photos());
   });
 
+  it('should retrieve user info', function () {
+    $httpBackend = injector.get('$httpBackend');
+    $httpBackend.whenJSONP(FEED_URL).respond({
+
+    });
+    instagramService = injector.get('instagramService');
+    expect(instagramService.user());
+  });
 });
