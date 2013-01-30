6143523347b15a122ac3b4e773542199a5c321d7
Sean Doyle <sean.p.doyle24@gmail.com>
2013-01-29 22:21:45 -0500

    touched up firefox display code

--- app/dep/angularjs-bootstrap/template/carousel/carousel.html.js
+++
@@ -1,9 +0,0 @@
-angular.module("template/carousel/carousel.html", []).run(["$templateCache", function($templateCache){
-  $templateCache.put("template/carousel/carousel.html",
-    "<div ng-mouseenter=\"pause()\" ng-mouseleave=\"play()\" class=\"carousel\">" +
-    "    <div class=\"carousel-inner\" ng-transclude></div>" +
-    "    <a ng-click=\"prev()\" class=\"carousel-control left\">&lsaquo;</a>" +
-    "    <a ng-click=\"next()\" class=\"carousel-control right\">&rsaquo;</a>" +
-    "</div>" +
-    "");
-}]);
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var casteApp = angular.module('casteApp', ['ui', 'ui.bootstrap'])
+var casteApp = angular.module('casteApp', ['ui'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/', {
